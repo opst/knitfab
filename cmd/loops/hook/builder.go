6704de689014270217b14c18a5e626eb0f099b0e
takaoka.youta <takaoka.youta@opst.co.jp>
2024-05-20 09:54:21 +0900

    remove unused type

--- cmd/loops/hook/builder.go
+++ cmd/loops/hook/builder.go
@@ -5,12 +5,6 @@ import (
 	cfg_hook "github.com/opst/knitfab/pkg/configs/hook"
 )
 
-type Hooks struct {
-	Initialize    Web[api_runs.Detail]
-	RunManagement Web[api_runs.Detail]
-	Finishing     Web[api_runs.Detail]
-}
-
 func Build(cfg cfg_hook.WebHook) Web[api_runs.Detail] {
 	return Web[api_runs.Detail]{
 		BeforeURL: cfg.Before,
